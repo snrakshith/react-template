9d76c3002f3cf8ae6ab5fd091665572ed0ce7b93
snrakshith <snrakshith.97@gmail.com>
2024-07-30 20:06:22 +0530

    initial commit

---
+++ src/App.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { App } from './App'
+
+const meta = {
+  title: 'Components/App',
+  component: App,
+} satisfies Meta<typeof App>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default = {} satisfies Story
